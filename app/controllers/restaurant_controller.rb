0382e05ae63be7fb56a86a6da4c23f4ba65ec463
Flo Leinenkugel <flolein@gmx.net>
2020-02-19 20:11:46 +0100

    created models, controllers, routes

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,13 @@
+class RestaurantController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dish',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '12546383',
+    category:       'french'
+  },
+  {
+    name:         'Dishoom',
+    address:      '10 dary St, fick E3 7pE',
+    phone_number:  '1253534',
+    category:       'indian'
+  },
+  {
+    name:         'bisham',
+    address:      '8 Bndary St, fuck d2 7JE',
+    phone_number:  '125353535',
+    category:       'chinese'
+  },
+  {
+    name:         'kalle',
+    address:      'jskjsksks',
+    phone_number:  '135533',
+    category:       'belgian'
+  },
+  {
+    name:         'mario',
+    address:      'sjsdj sjsjs sjsjs ',
+    phone_number:  '7474849',
+    category:       'italian'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
